1236bbd18eda0f4b1c9cc862faa4a9ef9ea4b07b
Allison Kosy <Allisonkosy@gmail.com>
2025-01-18 22:19:20 +0100

    add device preview

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,21 @@
+import 'package:device_preview_screenshot/device_preview_screenshot.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:testapp/screens/page_1.dart';
+import 'package:device_preview/device_preview.dart';
 
 void main() {
-  runApp(const ProviderScope(child: MyApp()));
+  runApp(
+    DevicePreview(
+      enabled: !kReleaseMode,
+      builder: (context) => const ProviderScope(child: MyApp()),
+          tools: const [
+        ...DevicePreview.defaultTools,
+        DevicePreviewScreenshot(),
+    ],
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -13,7 +25,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter Test App',
+      debugShowCheckedModeBanner: false,
+      locale: DevicePreview.locale(context),
+      builder: DevicePreview.appBuilder,
       theme: ThemeData(
       
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
